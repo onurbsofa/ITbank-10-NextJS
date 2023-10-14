3a6457bdb1883477abf8cf7a403a113d4ceac7e9
BoFo <brunofazio466@gmail.com>
2023-10-14 17:24:04 -0300

    lista cuantas con rutas estaticas

--- src/app/components/ListadoDeCuentas/ListadoDeCuentas.jsx
+++ src/app/components/ListadoDeCuentas/ListadoDeCuentas.jsx
@@ -1,15 +1,25 @@
-import Link from 'next/link';
+import Link from "next/link"
+async function getUsers() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const users = await res.json()
+ 
+  return users
+}
 
-export default function ListadoDeCuentas({ cuentas }) {
+
+export default async function ListadoDeCuentas({params}) {
+
+  const users = await getUsers()
+  console.log(users)
+
+
   return (
     <div>
       <h1>Listado de cuentas:</h1>
       <ul>
-        {cuentas.map((cuenta) => (
-          <li key={cuenta.id}>
-            <Link href={`/${cuenta.id}`}>
-              <a>{cuenta.nombre}</a>
-            </Link>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link href={`transferencias/${user.id}`}>{user.name}</Link>
           </li>
         ))}
       </ul>
@@ -17,27 +27,5 @@ export default function ListadoDeCuentas({ cuentas }) {
   );
 }
 
-export async function getStaticProps() {
-  
-  const res = await fetch('../../api/cuentasBancarias.json');
-  const cuentas = await res.json();
 
-  return {
-    props: {
-      cuentas,
-    },
-  };
-}
 
-export async function getStaticPaths() {
-
-  // Generate paths for each cuenta
-  const paths = cuentas.map((cuenta) => ({
-    params: { id: cuenta.id.toString() },
-  }));
-
-  return {
-    paths,
-    fallback: false,
-  };
-}
---
+++ src/app/transferencias/[id]/page.jsx
@@ -0,0 +1,25 @@
+
+
+ 
+export async function generateStaticParams() {
+    return [{ id: '1' }, { id: '2' }]
+  }
+   
+  async function getUser(params) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+    const user = await res.json()
+   
+    return user
+  }
+  
+  export default async function ListadoDeCuentas({params}) {
+  
+    const id = params.id
+    const user = await getUser(params)
+  return (
+        <div>
+
+            <h1>Cuenta de {user.name}</h1>
+        </div>
+  )
+}
