36cfbe3c1d11bce51e66444345230ee21173d201
BoFo <brunofazio466@gmail.com>
2023-10-28 11:30:26 -0300

    layout transferencias

---
+++ src/app/transferencias/layout.jsx
@@ -0,0 +1,12 @@
+
+
+const TransactionLayout = ({ children }) => {
+    return (
+        <div>LAyout del trans
+        {children}
+        
+        </div>
+    )
+};
+
+export default TransactionLayout;
